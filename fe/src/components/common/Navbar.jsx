c9eca10b3ca6a62944921252c45f6752b0f31602
Derek Rojas Mendoza <derek.rojas.mendoza@est.una.ac.cr>
2025-10-05 13:55:49 -0600

    commit

--- fe/src/components/common/Navbar.jsx
+++ fe/src/components/common/Navbar.jsx
@@ -1,31 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { Navbar as RBNavbar, Nav, Container } from "react-bootstrap";
 import { ROUTES } from "../../utils/constants";
+import "../../styles/styles.css";
 
 const Navbar = () => {
+  const location = useLocation(); // Para resaltar el link activo
+
   return (
-    <nav>
-      <div>
-        <Link to={ROUTES.HOME}>DrakeIn</Link>
-        <ul>
-          <li>
-            <Link to={ROUTES.HOME}>Inicio</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.RESERVATIONSHOTEL}>Ver Reservaciones</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.ROUTES}>Administrar Rutas</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.REGISTER}>Registrar Operador</Link>
-          </li>
-          <li>
-            <Link to={ROUTES.LOGIN}>Iniciar Sesión</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <RBNavbar bg="dark" variant="dark" expand="lg" className="navbar-blue mb-4">
+      <Container>
+        <RBNavbar.Brand as={Link} to={ROUTES.HOME}>
+          DrakeIn
+        </RBNavbar.Brand>
+        <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <RBNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              as={Link}
+              to={ROUTES.HOME}
+              active={location.pathname === ROUTES.HOME}
+            >
+              Inicio
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={ROUTES.RESERVATIONSHOTEL}
+              active={location.pathname === ROUTES.RESERVATIONSHOTEL}
+            >
+              Ver Reservaciones
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={ROUTES.ROUTES}
+              active={location.pathname === ROUTES.ROUTES}
+            >
+              Administrar Rutas
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={ROUTES.REGISTER}
+              active={location.pathname === ROUTES.REGISTER}
+            >
+              Registrar Operador
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={ROUTES.LOGIN}
+              active={location.pathname === ROUTES.LOGIN}
+            >
+              Iniciar Sesión
+            </Nav.Link>
+          </Nav>
+        </RBNavbar.Collapse>
+      </Container>
+    </RBNavbar>
   );
 };
 
